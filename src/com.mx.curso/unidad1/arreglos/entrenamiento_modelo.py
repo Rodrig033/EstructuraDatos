172b0e0f98c13dedb3ff907f1bfc911da87457ce
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2025-09-11 08:33:50 -0600

    Add files via upload

---
+++ src/com.mx.curso/unidad1/arreglos/entrenamiento_modelo.py
@@ -0,0 +1,23 @@
+# Metricas de precisión:
+
+metricas = [
+    0.75, 
+    0.90,
+    0.30,
+    0.12,
+    0.03,
+    0.33,
+    0.40,
+    0.78
+]
+
+# Agregar precisión:
+
+metricas.append(0.13)
+print("Metricas = ",metricas)
+
+# Posición final:
+print("Posición final: ",metricas[-1])
+
+# Posición más alta:
+print("Valor maximo: ",max(metricas))
