c6b9670f0deae42045faed5d8b93839e834423c2
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2025-10-07 08:26:59 -0600

    Migratory birds

--- src/com.mx.curso/unidad1/ejercicios_algebra/migratory_birds.py
+++ src/com.mx.curso/unidad1/ejercicios_algebra/migratory_birds.py
@@ -1,32 +1,30 @@
-# Aves migratorias:
 migratory_birds = [1, 1, 2, 2, 3]
 tipos = []
-repetidos = []
-tipo_pequeño = 0
+conteo_repetidos = []
 
-# Almacenar solo los tipos:
-for i in range(len(migratory_birds)):
-    for j in range(len(migratory_birds)):
-        if i != j:
-            if migratory_birds[i] != migratory_birds[j] and migratory_birds[i] not in tipos:
-                tipos.append(migratory_birds[i])
+# Obtener tipos
+for i in migratory_birds:
+    if i not in tipos:
+        tipos.append(i)
 
-# Repetidos
-for i in range(len(migratory_birds)):
-    for j in range(len(migratory_birds)):
-        if i != j:
-            if migratory_birds[i] == migratory_birds[j] and migratory_birds[i] not in repetidos:
-                repetidos.append(migratory_birds[i])
+# Contar repeticiones
+for tipo in tipos:
+    contador = 0
+    for ave in migratory_birds:
+        if ave == tipo:
+            contador += 1
+    conteo_repetidos.append(contador)
 
-# Recorrer el arreglo y comparar para conseguir el valor pequeño:
+# Tipo más repetido
+max_repeticiones = max(conteo_repetidos)
+tipos_maximos = []
 for i in range(len(tipos)):
-    for j in range(len(tipos)):
-        if i != j:
-            if migratory_birds[i] < migratory_birds[j]:
-                tipo_pequeño = migratory_birds[i]
+    if conteo_repetidos[i] == max_repeticiones:
+        tipos_maximos.append(tipos[i])
 
+tipo_pequeño = min(tipos_maximos)
 
-# Valores: 
-print("Tipos de aves: ", tipos)
-print("Repetidos: ",repetidos)
-print("Tipo pequeño: ", tipo_pequeño)
+print("Tipos de aves:", tipos)
+print("Conteo de repeticiones:", conteo_repetidos)
+print("Tipos más comunes:", tipos_maximos)
+print("Tipo más pequeño:", tipo_pequeño)
