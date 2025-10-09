67f37340186401ce651a72a29a2dfd7605d2148c
Rodrigo Farid <rodrigolopezcordoba805@gmail.com>
2025-10-09 07:12:21 -0600

    Migratory Bird

--- src/com.mx.curso/unidad1/ejercicios_algebra/migratory_birds.py
+++ src/com.mx.curso/unidad1/ejercicios_algebra/migratory_birds.py
@@ -1,4 +1,4 @@
-migratory_birds = [1, 1, 2, 2, 3]
+migratory_birds = [1, 4, 4, 4, 5, 3]
 tipos = []
 conteo_repetidos = []
 
@@ -27,4 +27,4 @@ tipo_pequeño = min(tipos_maximos)
 print("Tipos de aves:", tipos)
 print("Conteo de repeticiones:", conteo_repetidos)
 print("Tipos más comunes:", tipos_maximos)
-print("Tipo más pequeño:", tipo_pequeño)
+print("Tipo más pequeño:", tipo_pequeño)  
